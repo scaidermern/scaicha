5cf0a2483e8c75349cee027a3ee159d7bb4f0b3a
Alexander Heinlein <alexander.heinlein@web.de>
2013-03-22 15:50:25 +0100

    added option to specify minimum tag occurence ratio

--- find_smiliar_tags.py
+++ find_smiliar_tags.py
@@ -30,27 +30,36 @@ from itertools import combinations
 
 def printUsage(name):
     print "usage:", name, "[OPTIONS] [FILES]"
-    print "  -f      , --fast          fast mode with less accuracy"
-    print "  -r <arg>, --ratio <arg>   minimum similarity ratio (0..1) (default: 0.9)"
-    print "  -h      , --help          print this help and exit"
+    print "  -f      , --fast           fast mode with less accuracy"
+    print "  -r <arg>, --ratio <arg>    minimum similarity ratio (0..1) (default: 0.9)"
+    print "  -o <arg>, --occRatio <arg> minimum occurence ratio (0..1) (default: 0.05)"
+    print "                             for the more common tag, less occuring tags"
+    print "                             won't be reported"
+    print "  -h      , --help           print this help and exit"
     print
 
 if __name__ == '__main__':
     try:
         # get command line arguments
-        opts, args = getopt.getopt(sys.argv[1:], "fr:h", ["fast", "ratio=", "help"])
+        opts, args = getopt.getopt(sys.argv[1:], "fr:o:h", ["fast", "ratio=", "occRatio=", "help"])
     except getopt.GetoptError:
         printUsage(sys.argv[0])
         raise RuntimeError, "invalid argument specified"
     
     fast = False
     minSimilarityRatio = 0.9
+    minOccurenceRatio = 0.05
     for opt, arg in opts:
         if opt in ("-f", "--fast"):
             fast = True
         elif opt in ("-r", "--ratio"):
             try:
                 minSimilarityRatio = float(arg)
+            except ValueError:
+                raise RuntimeError, "ratio argument '" + arg + "' is not a float!"
+        elif opt in ("-o", "--occRatio"):
+            try:
+                minOccurenceRatio = float(arg)
             except ValueError:
                 raise RuntimeError, "ratio argument '" + arg + "' is not a float!"
         elif opt in ("-h", "--help"):
@@ -64,13 +73,14 @@ if __name__ == '__main__':
         raise RuntimeError, "no files specified!"
     
     # parse tag dumps
-    tagSet = defaultdict(int)
+    tagSet = defaultdict(float)
     for file in files:
         print "parsing file", file
         file = open(file, "r")
         for line in file:
             line = line.rstrip("\n")
             ratio, name = line.split("\t")
+            ratio = float(ratio)
             if name not in tagSet:
                 tagSet[name] = ratio
             else:
@@ -79,6 +89,7 @@ if __name__ == '__main__':
 
     # compare each tag with each other one
     print "searching for similar tags (this will take some time)"
+    print "note: reported occurence ratios are the per user ratios and not the global last.fm ratios!"
     for tag, otherTag in combinations(tagSet, 2):
         similarityRatio = None
         if not fast:
@@ -95,5 +106,7 @@ if __name__ == '__main__':
             else:
                 moreCommonTag, moreCommonTagOccurenceRatio = otherTag, otherTagOccurenceRatio
                 lessCommonTag, lessCommonTagOccurenceRatio = tag, tagOccurenceRatio
-            print "'%s' (%s%%) and '%s' (%s%%) have a similarity ratio of %s" % (moreCommonTag, moreCommonTagOccurenceRatio, lessCommonTag, lessCommonTagOccurenceRatio, similarityRatio)
+            
+            if moreCommonTagOccurenceRatio >= minOccurenceRatio:
+                print "'%s' (%s%%) and '%s' (%s%%) have a similarity ratio of %s" % (moreCommonTag, moreCommonTagOccurenceRatio, lessCommonTag, lessCommonTagOccurenceRatio, similarityRatio)
     
